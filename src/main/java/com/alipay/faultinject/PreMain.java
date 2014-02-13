2d14bb2faf3198af1308c910355d484a1de987c7
yiming.wym <yiming.wym@alipay.com>
2014-02-13 14:19:54 +0800

    init project

---
+++ src/main/java/com/alipay/faultinject/PreMain.java
@@ -0,0 +1,18 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2014 All Rights Reserved.
+ */
+package com.alipay.faultinject;
+
+/**
+ * 
+ * @author yimingwym 
+ * @version $Id: PreMain.java, v 0.1 2014-2-13 обнГ1:49:00 yimingwym Exp $
+ */
+public class PreMain {
+
+    public static void premain(String agentArgs) {
+        System.out.println("in premain method");
+    }
+
+}
