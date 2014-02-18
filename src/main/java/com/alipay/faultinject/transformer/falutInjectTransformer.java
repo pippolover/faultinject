46ffe67dd0e470ce49d204c3eff6ce2f059ec094
yiming.wym <yiming.wym@alipay.com>
2014-02-18 22:04:15 +0800

    add checkClass logic

--- src/main/java/com/alipay/faultinject/transformer/falutInjectTransformer.java
+++ src/main/java/com/alipay/faultinject/transformer/falutInjectTransformer.java
@@ -7,6 +7,7 @@ package com.alipay.faultinject.transformer;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
@@ -14,6 +15,7 @@ import java.security.ProtectionDomain;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.util.CheckClassAdapter;
 
 import com.alipay.faultinject.asm.adapter.faultInjectAdapter;
 
@@ -53,10 +55,13 @@ public class falutInjectTransformer implements ClassFileTransformer {
                 cr.accept(classVisitor, ClassReader.SKIP_DEBUG);
 
                 byte[] classFile = cw.toByteArray();
-                //                CheckClassAdapter.verify(cr, false, new PrintWriter(System.out));
+                CheckClassAdapter.verify(new ClassReader(classFile), false, new PrintWriter(
+                    System.out));
                 storeClassFile("/home/admin/logs/" + rawName + ".class", classFile);
                 return classFile;
             } catch (IOException e) {
+                e.printStackTrace();
+            } catch (Throwable e) {
                 e.printStackTrace();
             }
 
